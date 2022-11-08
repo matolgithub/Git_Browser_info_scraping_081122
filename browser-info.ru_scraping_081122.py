920ff2f5d5c685b6f9d17a9306df4161dbcc0622
matolgithub <89175955650@mail.ru>
2022-11-08 20:07:59 +0300

    Next commit to Git.

--- browser-info.ru_scraping_081122.py
+++ browser-info.ru_scraping_081122.py
@@ -1,13 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
+from requests import get
+from bs4 import BeautifulSoup as bs
 from pprint import pprint
 from fake_headers import Headers
 from fake_user_agent import user_agent
 
+url = "https://browser-info.ru/"
+header = Headers().generate()
+
 
 def main():
-    pass
-
+    response = get(url=url, headers=header).text
+    # pprint(response)
+    soup = bs(response, "lxml")
+    # pprint(soup.text)
+    block = soup.find("div", id="javascript_check")
+    # pprint(block)
+    find_status_js = block.find_all("span")
+    pprint(f"----------{find_status_js[0].text} ------------------- {find_status_js[1].text}.")
 
 if __name__ == "__main__":
     main()
