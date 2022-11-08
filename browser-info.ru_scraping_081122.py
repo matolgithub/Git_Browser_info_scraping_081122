6e9131a4c5de9395ada80efd5ba046029da7efdf
matolgithub <89175955650@mail.ru>
2022-11-08 19:34:28 +0300

    First commit to Git.

---
+++ browser-info.ru_scraping_081122.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+from fake_headers import Headers
+from fake_user_agent import user_agent
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
